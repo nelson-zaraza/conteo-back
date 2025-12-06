3ca7b6a815d1cd4e1599a9d22338213de7e63c93
Nelson Mauricio <nelsonnavarro@tmtek.net>
2025-12-06 00:25:13 -0500

    se agrega la politica de cors

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://lighthearted-lily-6ab87d.netlify.app", // tu frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
